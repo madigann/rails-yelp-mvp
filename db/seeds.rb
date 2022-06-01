6547c46f6afc510d02a7393ae4c664970fb72125
Noel Madigan <nmadigan@industrial-integration.com>
2022-06-01 08:04:08 +0000

    Commit for Rails Yelp MVP challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+restaurant_ids = []
+
+
+puts "Cleaning restaurant database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+10.times do
+  restaurant = Restaurant.create(  name: Faker::Restaurant.name,
+                      address: Faker::Address.street_name,
+                      phone_number: Faker::PhoneNumber.phone_number,
+                      category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+  restaurant_ids.append restaurant.id
+end
+puts "Finished creating restaurants!"
+
+
+puts "Cleaning review database..."
+Review.destroy_all
+
+puts "Creating reviews..."
+10.times do
+  Review.create(restaurant: Restaurant.find(restaurant_ids.sample), rating: Faker::Number.between(from: 0, to: 5), content: Faker::Restaurant.review)
+end
+puts "Finished creating reviews!"
